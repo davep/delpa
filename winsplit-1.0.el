020e0fc5c99d082957d299634afac9712bad1e76
Dave Pearson <davep@davep.org>
2017-04-10 08:11:43 +0100

    Add winsplit.el

---
+++ winsplit-1.0.el
@@ -0,0 +1,41 @@
+;;; winsplit.el --- Simple commands for creating new windows
+;; Copyright 2017 by Dave Pearson <davep@davep.org>
+
+;; Author: Dave Pearson <davep@davep.org>
+;; Version: 1.0
+;; Keywords: convenience
+;; URL: https://github.com/davep/winsplit.el
+
+;;; Commentary:
+;;
+;; winsplit.el provides some simple commands for creating new windows in the
+;; way I like. The commands are designed to be assigned to obvious key
+;; bindings.
+
+;;; Code:
+
+(defun winsplit-left ()
+  "Create a new focused window to the left of the current window."
+  (interactive)
+  (split-window-horizontally))
+
+(defun winsplit-right ()
+  "Create a new focused window to the right of the current window."
+  (interactive)
+  (winsplit-left)
+  (other-window 1))
+
+(defun winsplit-above ()
+  "Create a new focused window above the current window."
+  (interactive)
+  (split-window-vertically))
+
+(defun winsplit-below ()
+  "Create a new focused window below the current window."
+  (interactive)
+  (winsplit-above)
+  (other-window 1))
+
+(provide 'winsplit)
+
+;;; winsplit.el ends here
---
+++ winsplit-readme.txt
@@ -0,0 +1,6 @@
+;;; Commentary:
+;;
+;; winsplit.el provides some simple commands for creating new windows in the
+;; way I like. The commands are designed to be assigned to obvious key
+;; bindings.
+
