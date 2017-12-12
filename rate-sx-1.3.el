ddd88effea6f8972e9e6d3239aae721cc43bee91
Dave Pearson <davep@davep.org>
2017-12-12 13:49:46 +0000

    Update rate-sx.el

--- rate-sx-1.3.el
+++ rate-sx-1.3.el
@@ -2,9 +2,10 @@
 ;; Copyright 2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.2
+;; Version: 1.3
 ;; Keywords: comm, currency, bitcoin, money
 ;; URL: https://github.com/davep/rate-sx.el
+;; Package-Requires: ((emacs "24.4"))
 
 ;; This program is free software: you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by the
@@ -21,13 +22,34 @@
 
 ;;; Commentary:
 ;;
-;; rate-sx.el provides a command for showing currency rates from rate.sx.
+;; `rate-sx.el' provides a main command, `rate-sx', which displays the
+;; output of http://rate.sx in a buffer.
+;;
+;; Other commands provide ways to quickly calculate currency totals, they
+;; include:
+;;
+;; `rate-sx-calc'
+;;
+;; Show the result of a currency calculation in the minibuffer. Calculations
+;; are things like "1BTC+12ETH" (would show the total value, in the base
+;; currency defined by `rate-sx-default-currency', of holding 1 BTC and 12
+;; ETH).
+;;
+;; `rate-sx-calc-region'
+;;
+;; Same as above but takes the input from the content of the marked region.
+;;
+;; `rate-sx-calc-maybe-region'
+;;
+;; Same as above again, but performs `rate-sx-calc-region' if there is an
+;; active mark, otherwise it performs `rate-sx-calc'.
 
 ;;; Code:
 
 (require 'ansi-color)
+(require 'url-util)
 
-(defconst rate-sx-url "http://%srate.sx/"
+(defconst rate-sx-url "http://%srate.sx/%s"
   "URL for rate.sx.")
 
 (defconst rate-sx-user-agent "rate-sx.el (https://github.com/davep/rate-sx.el) (curl)"
@@ -59,11 +81,13 @@ If nil the default currency as used by rate.sx itself will be
 used. See `rate-sx-currencies' or the help screen of rate.sx
 itself for more currency options.")
 
-(defun rate-sx-get (&optional currency)
+(defun rate-sx-get (&optional currency params)
   "Get the output from rate.sx.
 
 Values will be acquired in CURRENCY, or the default currency of
-rate.sx will be used if one isn't supplied."
+rate.sx will be used if one isn't supplied.
+
+PARAMS will be added to the end of `rate-sx-url' if they are supplied."
   (let* ((url-mime-accept-string "text/plain")
          (url-request-extra-headers `(("User-Agent" . ,rate-sx-user-agent)))
          (url-show-status nil))
@@ -71,9 +95,11 @@ rate.sx will be used if one isn't supplied."
       (url-insert-file-contents (format rate-sx-url
                                         (if currency
                                             (concat currency ".")
-                                          "")))
+                                          "")
+                                        (or params "")))
       (buffer-string))))
 
+;;;###autoload
 (defun rate-sx (currency)
   "Show the current output of rate.sx in a new buffer.
 
@@ -85,6 +111,28 @@ If CURRENCY is non-nil, this command will prompt for a display currency."
   (with-help-window rate-sx-buffer
     (with-current-buffer rate-sx-buffer
       (insert (ansi-color-apply (rate-sx-get currency))))))
+
+;;;###autoload
+(defun rate-sx-calc (calc)
+  "Evaluate CALC via rate.sx.
+
+The result is given in `rate-sx-default-currency'."
+  (interactive "sCalc: ")
+  (message "Result: %s" (string-trim (rate-sx-get rate-sx-default-currency (url-hexify-string calc)))))
+
+;;;###autoload
+(defun rate-sx-calc-region (start end)
+  "Perform `rate-sx-calc' on text in region START to END."
+  (interactive "r")
+  (rate-sx-calc (buffer-substring-no-properties start end)))
+
+;;;###autoload
+(defun rate-sx-calc-maybe-region ()
+  "Perform a rate calculation on a region if one is active.
+
+If one isn't active, prompt for the calculation."
+  (interactive)
+  (call-interactively (if mark-active #'rate-sx-calc-region #'rate-sx-calc)))
 
 (provide 'rate-sx)
 
