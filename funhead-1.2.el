63ccc30084c5a194c7f50166b3d1e7b8496f8a53
Dave Pearson <dave.pearson@synpromics.com>
2019-03-07 13:19:39 +0000

    Update funhead.el

--- funhead-1.2.el
+++ funhead-1.2.el
@@ -2,7 +2,7 @@
 ;; Copyright 2019 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.1
+;; Version: 1.2
 ;; Keywords: convenience
 ;; URL: https://github.com/davep/funhead.el
 ;; Package-Requires: ((emacs "24"))
@@ -30,7 +30,7 @@
 
 (defun funhead--format ()
   "Return the correct `funhead' format for the current buffer."
-  (cond ((derived-mode-p 'python-mode 'sh-mode)
+  (cond ((derived-mode-p 'python-mode 'sh-mode 'makefile-gmake-mode)
          (lambda ()
            (insert (concat (make-string 78 ?#) "\n# "))))
         ((derived-mode-p 'js-mode 'css-mode)
