40da9971d085fce351ab315cb8817f725818b844
Dave Pearson <dave.pearson@synpromics.com>
2019-05-17 11:10:35 +0100

    Update end-it.el

--- end-it-1.16.el
+++ end-it-1.16.el
@@ -2,7 +2,7 @@
 ;; Copyright 2018-2019 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.15
+;; Version: 1.16
 ;; Keywords: convenience
 ;; URL: https://github.com/davep/end-it.el
 ;; Package-Requires: ((emacs "24"))
@@ -42,6 +42,8 @@
          "/* %s */")
         ((derived-mode-p 'rust-mode)
          "// %s")
+        ((derived-mode-p 'org-mode)
+         "# %s")
         ((derived-mode-p 'markdown-mode)
          "[//]: # (%s)")
         ((derived-mode-p 'web-mode)
