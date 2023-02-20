d44fe73934bb4f3a307bb90166a5513c08f7dbc0
Dave Pearson <davep@davep.org>
2023-02-20 09:27:35 +0000

    :sparkles: Add reframe.el

---
+++ reframe-1.0.el
@@ -0,0 +1,49 @@
+;;; reframe.el --- Size/position frames depending where I am -*- lexical-binding: t -*-
+;; Copyright 2023 by Dave Pearson <davep@davep.org>
+
+;; Author: Dave Pearson <davep@davep.org>
+;; Version: 1.0
+;; Keywords: convenience, frames
+;; URL: https://github.com/davep/reframe.el
+;; Package-Requires: ((emacs "24"))
+
+;; This program is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation, either version 3 of the License, or (at your
+;; option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+;; Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License along
+;; with this program. If not, see <http://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;;
+;; reframe.el is a personal helper package that lets me quickly and easily
+;; position a frame depending on context.
+
+;;; Code:
+
+(require 'is-a)
+
+(defconst reframe-rules
+  '(((lambda ()
+       (and is-a-macOS-window-p (string= (downcase (system-name)) "lucien.local"))) . (100 40 300 110)))
+  "List of rules for reframing.")
+
+;;;###autoload
+(defun reframe ()
+  "Move and resize the current frame depending on context."
+  (interactive)
+  (mapcar (lambda (rule)
+            (when (funcall (car rule))
+              (set-frame-position (selected-frame) (cadr rule) (caddr rule))
+              (set-frame-size (selected-frame) (cadddr rule) (car (cddddr rule)))))
+          reframe-rules))
+
+(provide 'reframe)
+
+;;; reframe.el ends here
---
+++ reframe-readme.txt
@@ -0,0 +1,3 @@
+
+reframe.el is a personal helper package that lets me quickly and easily
+position a frame depending on context.
